d773636de4509f253acda6ab86b952eab53ebb49
MariiaBakhmat <105664435+MariiaBakhmat@users.noreply.github.com>
2025-09-22 19:05:48 +0300

    Update index.js

--- src/widget/index.js
+++ src/widget/index.js
@@ -151,5 +151,5 @@ import './styles/widget.scss';
 })();
 
 // ES6 експорт для модульного використання (якщо потрібно)
-export { GewurzWidget, CONFIG } from './Widget.js';
-export { CONFIG} from './config.js';
+export { GewurzWidget } from './Widget.js';
+export { CONFIG } from './config.js';
